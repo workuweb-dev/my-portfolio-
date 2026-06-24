15262bc852050a5e48e35580a80c864aa2af7b8c
workuweb-dev <wabebayehu9@gmail.com>
2026-06-24 13:03:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,84 @@
-const card = document.querySelector(".card");
+body{
 
-card.addEventListener("mousemove",(e)=>{
-    card.style.transform =
-    "perspective(1000px) rotateY(10deg)";
-});
+margin:0;
 
-card.addEventListener("mouseleave",()=>{
-    card.style.transform =
-    "perspective(1000px) rotateY(-10deg)";
-});
+font-family:Arial;
+
+background:#0f172a;
+
+color:white;
+
+}
+
+
+.hero{
+
+height:100vh;
+
+display:flex;
+
+justify-content:center;
+
+align-items:center;
+
+}
+
+
+
+.glass{
+
+padding:50px;
+
+border-radius:25px;
+
+background:rgba(255,255,255,0.1);
+
+backdrop-filter:blur(15px);
+
+text-align:center;
+
+box-shadow:0 0 40px rgba(0,0,0,.4);
+
+}
+
+
+
+button{
+
+padding:15px 40px;
+
+border:none;
+
+border-radius:30px;
+
+background:#38bdf8;
+
+color:white;
+
+cursor:pointer;
+
+}
+
+
+
+section{
+
+padding:80px;
+
+text-align:center;
+
+}
+
+
+
+.projects p{
+
+background:#1e293b;
+
+padding:20px;
+
+margin:10px;
+
+border-radius:15px;
+
+}
